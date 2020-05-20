39cac1688c85a4c7912dcf4ca3b66d2174951d2b
Nikolaus Demmel <demmeln@in.tum.de>
2020-05-20 22:40:11 +0200

    add 20.04

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@
 . config.sh
 
 for t in $TAGS; do
-    docker build -t $REPO:$t -f Dockerfile_$t .
+    docker build --pull -t $REPO:$t -f Dockerfile_$t .
     docker tag $REPO:$t $ORG/$REPO:$t
     if [ $t = $LATEST ]; then
        docker tag $REPO:$t $REPO:latest
--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
 ORG=nikolausdemmel
 REPO=ubuntu-dev-cv-gui
-TAGS="18.04 16.04 14.04"
-LATEST=18.04
+TAGS="20.04 18.04 16.04 14.04"
+LATEST=20.04
